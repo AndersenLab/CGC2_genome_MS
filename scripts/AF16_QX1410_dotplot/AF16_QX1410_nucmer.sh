f095cf917f4cfc4659f3efa7a0ae26cc7ac43d03
lanceman999 <loconn13@jh.edu>
2026-02-18 10:39:52 -0500

    udpates to file path calling in alignment script

--- scripts/AF16_QX1410_dotplot/AF16_QX1410_nucmer.sh
+++ scripts/AF16_QX1410_dotplot/AF16_QX1410_nucmer.sh
@@ -10,7 +10,7 @@
 source activate mummer
 
 # align with nucmer (will spit out a .delta file)
-nucmer --maxgap=500 --prefix=AF16_contigs --coords QX1410.ONT.fa AF16.WS276.fa 
+nucmer --maxgap=500 --prefix=AF16 --coords ../../raw_data/genomes/c_briggsae.QX1410_nanopore.Feb2020.genome.fa ../../raw_data/genomes/c_briggsae.PRJNA10731.WS276.AF16.genome.fa
 
 # get coordinate file - filter to contain high-quality alignments that are >1kb 
-show-coords -r -l -T AF16_contigs.delta | awk '$5 > 1000' > QX1410_AF16_transformed.tsv
+show-coords -r -l -T AF16.delta | awk '$5 > 1000' > ../../processed_data/genome_genome_alignments/AF16_QX1410.transformed.tsv
