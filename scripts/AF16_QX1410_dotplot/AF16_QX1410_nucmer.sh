664e14930259fb8d85b8785e64b9cdda3d6cf9be
lanceman999 <loconn13@jh.edu>
2026-02-20 10:08:54 -0500

    slight updates

--- scripts/AF16_QX1410_dotplot/AF16_QX1410_nucmer.sh
+++ scripts/AF16_QX1410_dotplot/AF16_QX1410_nucmer.sh
@@ -14,3 +14,5 @@ nucmer --maxgap=500 --prefix=AF16 --coords ../../raw_data/genomes/c_briggsae.QX1
 
 # get coordinate file - filter to contain high-quality alignments that are >1kb 
 show-coords -r -l -T AF16.delta | awk '$5 > 1000' > ../../processed_data/genome_genome_alignments/AF16_QX1410.transformed.tsv
+
+sed -i '1d' ../../processed_data/genome_genome_alignments/AF16_QX1410.transformed.tsv
