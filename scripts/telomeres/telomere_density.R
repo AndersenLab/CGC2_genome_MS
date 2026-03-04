76164e8aece483ddae535fac5603f4ef3ea6f918
lanceman999 <loconn13@jh.edu>
2026-03-04 10:35:56 -0500

    updated telomere scripts to match repo directory structure

--- scripts/telomeres/telomere_density.R
+++ scripts/telomeres/telomere_density.R
@@ -2,151 +2,91 @@ library(ggplot2)
 library(readr)
 library(dplyr)
 
-telomere_count <- readr::read_tsv("/vast/eande106/projects/Lance/THESIS_WORK/assemblies/CGC2_HiC/telomeres/cgc2_FINAL_telomeres_binned_1kb.bed", col_names = c("chrom","start","end","count"))
-
+# Desired chromosome ordering for plotting
 chrom_order <- c("I","II","III","IV","V","X")
 
-tel_plt <- telomere_count %>% dplyr::mutate(chrom = factor(chrom, levels = chrom_order), mid_Mb = (start + end) / 2 / 1e6)
+# Laod in CGC2 telomere counts
+tel_ct_CGC2 <- readr::read_tsv("../../processed_data/genomes/CGC2_telomeres_binned_1kb.bed", col_names = c("chrom","start","end","count")) %>%
+  dplyr::mutate(chrom = factor(chrom, levels = chrom_order), mid_Mb = (start + end) / 2 / 1e6) %>%
+  dplyr::filter(!grepl(">AC186293.1","chrom"))
 
-# ggplot(data = tel_plt) +
-#   geom_col(aes(x = mid_Mb, y = count), color = 'black', width = 0.0001) +
-#   facet_wrap(~chrom, nrow = 1, scales = "free") +
-#   theme_bw() +
-#   theme(
-#     panel.border = element_rect(color = 'black', fill = NA),
-#     strip.text = element_text(size = 14, color = 'black'),
-#     axis.text = element_text(size = 12, color = 'black'),
-#     axis.title = element_text(size = 14, color = 'black')
-#   ) +
-#   labs(x = "CGC2 genome position (Mb)", y = "Counts of TTAGGC per kb") +
-#   scale_y_continuous(expand = expansion(mult = c(0,0.1))) +
-#   coord_cartesian(ylim = c(0,180))
+# Laod in AF16 telomere counts
+tel_ct_AF16 <- readr::read_tsv("/vast/eande106/projects/Lance/THESIS_WORK/assemblies/CGC2_HiC/telomeres/af_telomeres_binned_1kb.bed", col_names = c("chrom","start","end","count")) %>%
+  dplyr::filter(chrom != "MtDNA" & !grepl("cb25", chrom)) %>%
+  dplyr::mutate(chrom = factor(chrom, levels = chrom_order), mid_Mb = (start + end) / 2 / 1e6)
 
+# Laod in QX1410 telomere counts
+tel_ct_QX1410 <- readr::read_tsv("/vast/eande106/projects/Lance/THESIS_WORK/assemblies/CGC2_HiC/telomeres/QX_telomeres_binned_1kb.bed", col_names = c("chrom","start","end","count")) %>%
+  dplyr::filter(chrom != "MtDNA") %>%
+  dplyr::mutate(chrom = factor(chrom, levels = chrom_order), mid_Mb = (start + end) / 2 / 1e6)
 
-vx_telomere_count <- readr::read_tsv("/vast/eande106/projects/Lance/THESIS_WORK/assemblies/CGC2_HiC/telomeres/VX_telomeres_binned_1kb.bed", col_names = c("chrom","start","end","count"))
+# Laod in VX34 telomere counts
+tel_ct_VX34 <- readr::read_tsv("/vast/eande106/projects/Lance/THESIS_WORK/assemblies/CGC2_HiC/telomeres/VX_telomeres_binned_1kb.bed", col_names = c("chrom","start","end","count")) %>%
+  dplyr::mutate(chrom = factor(chrom, levels = chrom_order), mid_Mb = (start + end) / 2 / 1e6)
 
-tel_plt_vx <- vx_telomere_count %>% dplyr::mutate(chrom = factor(chrom, levels = chrom_order), mid_Mb = (start + end) / 2 / 1e6)
-
-# ggplot(data = tel_plt_vx) +
-#   geom_col(aes(x = mid_Mb, y = count), color = 'red', width = 0.0001) +
-#   facet_wrap(~chrom, nrow = 1, scales = "free_x") +
-#   theme_bw() +
-#   theme(
-#     panel.border = element_rect(color = 'black', fill = NA),
-#     strip.text = element_text(size = 14, color = 'black'),
-#     axis.text = element_text(size = 12, color = 'black'),
-#     axis.title = element_text(size = 14, color = 'black')
-#   ) +
-#   labs(x = "VX34 genome position (Mb)", y = "Counts of TTAGGC per kb") +
-#   scale_y_continuous(expand = expansion(mult = c(0,0.1))) +
-#   coord_cartesian(ylim = c(0,180))
-
-
-
-qx_telomere_count <- readr::read_tsv("/vast/eande106/projects/Lance/THESIS_WORK/assemblies/CGC2_HiC/telomeres/QX_telomeres_binned_1kb.bed", col_names = c("chrom","start","end","count")) %>%
-  dplyr::filter(chrom != "MtDNA")
-
-tel_plt_qx <- qx_telomere_count %>% dplyr::mutate(chrom = factor(chrom, levels = chrom_order), mid_Mb = (start + end) / 2 / 1e6)
-
-# ggplot(data = tel_plt_qx) +
-#   geom_col(aes(x = mid_Mb, y = count), color = 'forestgreen', width = 0.0001) +
-#   facet_wrap(~chrom, nrow = 1, scales = "free_x") +
-#   theme_bw() +
-#   theme(
-#     panel.border = element_rect(color = 'black', fill = NA),
-#     strip.text = element_text(size = 14, color = 'black'),
-#     axis.text = element_text(size = 12, color = 'black'),
-#     axis.title = element_text(size = 14, color = 'black')
-#   ) +
-#   labs(x = "QX1410 genome position (Mb)", y = "Counts of TTAGGC per kb") +
-#   scale_y_continuous(expand = expansion(mult = c(0,0.1))) +
-#   coord_cartesian(ylim = c(0,180))
-
-
-
-af_telomere_count <- readr::read_tsv("/vast/eande106/projects/Lance/THESIS_WORK/assemblies/CGC2_HiC/telomeres/af_telomeres_binned_1kb.bed", col_names = c("chrom","start","end","count")) %>%
-  dplyr::filter(chrom != "MtDNA" & !grepl("cb25", chrom))
-
-tel_plt_af <- af_telomere_count %>% dplyr::mutate(chrom = factor(chrom, levels = chrom_order), mid_Mb = (start + end) / 2 / 1e6)
-
-# ggplot(data = tel_plt_af) +
-#   geom_col(aes(x = mid_Mb, y = count), color = 'seagreen', width = 0.0001) +
-#   facet_wrap(~chrom, nrow = 1, scales = "free_x") +
-#   theme_bw() +
-#   theme(
-#     panel.border = element_rect(color = 'black', fill = NA),
-#     strip.text = element_text(size = 14, color = 'black'),
-#     axis.text = element_text(size = 12, color = 'black'),
-#     axis.title = element_text(size = 14, color = 'black')
-#   ) +
-#   labs(x = "AF16 genome position (Mb)", y = "Counts of TTAGGC per kb") +
-#   scale_y_continuous(expand = expansion(mult = c(0,0.1))) +
-#   coord_cartesian(ylim = c(0,180))
-
-
-
-
-
-
-
-
-cowplot::plot_grid(
-  ggplot(data = tel_plt) +
+# Plotting telomere density
+tel_count_plt <- cowplot::plot_grid(
+  ggplot(data = tel_ct_CGC2) +
     geom_col(aes(x = mid_Mb, y = count), color = 'black', width = 0.0001) +
     facet_wrap(~chrom, nrow = 1, scales = "free") +
     theme_bw() +
     theme(
       panel.border = element_rect(color = 'black', fill = NA),
-      strip.text = element_text(size = 14, color = 'black'),
-      axis.text = element_text(size = 12, color = 'black'),
-      axis.title = element_text(size = 14, color = 'black')
+      strip.text = element_text(size = 11, color = 'black'),
+      axis.text = element_text(size = 10, color = 'black'),
+      axis.title = element_text(size = 11, color = 'black')
     ) +
     labs(x = "CGC2 genome position (Mb)", y = "Counts of TTAGGC per kb") +
     scale_y_continuous(expand = expansion(mult = c(0,0.1))) +
     coord_cartesian(ylim = c(0,180)),
   
-  ggplot(data = tel_plt_af) +
+  ggplot(data = tel_ct_AF16) +
     geom_col(aes(x = mid_Mb, y = count), color = 'blue', width = 0.0001) +
     facet_wrap(~chrom, nrow = 1, scales = "free_x") +
     theme_bw() +
     theme(
       panel.border = element_rect(color = 'black', fill = NA),
-      strip.text = element_text(size = 14, color = 'black'),
-      axis.text = element_text(size = 12, color = 'black'),
-      axis.title = element_text(size = 14, color = 'black')
+      strip.text = element_text(size = 11, color = 'black'),
+      axis.text = element_text(size = 10, color = 'black'),
+      axis.title = element_text(size = 11, color = 'black')
     ) +
     labs(x = "AF16 genome position (Mb)", y = "Counts of TTAGGC per kb") +
     scale_y_continuous(expand = expansion(mult = c(0,0.1))) +
     coord_cartesian(ylim = c(0,180)),
   
-  ggplot(data = tel_plt_qx) +
+  ggplot(data = tel_ct_QX1410) +
     geom_col(aes(x = mid_Mb, y = count), color = 'forestgreen', width = 0.0001) +
     facet_wrap(~chrom, nrow = 1, scales = "free_x") +
     theme_bw() +
     theme(
       panel.border = element_rect(color = 'black', fill = NA),
-      strip.text = element_text(size = 14, color = 'black'),
-      axis.text = element_text(size = 12, color = 'black'),
-      axis.title = element_text(size = 14, color = 'black')
+      strip.text = element_text(size = 11, color = 'black'),
+      axis.text = element_text(size = 10, color = 'black'),
+      axis.title = element_text(size = 11, color = 'black')
     ) +
     labs(x = "QX1410 genome position (Mb)", y = "Counts of TTAGGC per kb") +
     scale_y_continuous(expand = expansion(mult = c(0,0.1))) +
     coord_cartesian(ylim = c(0,180)),
   
-  ggplot(data = tel_plt_vx) +
+  ggplot(data = tel_ct_VX34) +
     geom_col(aes(x = mid_Mb, y = count), color = 'purple', width = 0.0001) +
     facet_wrap(~chrom, nrow = 1, scales = "free_x") +
     theme_bw() +
     theme(
       panel.border = element_rect(color = 'black', fill = NA),
-      strip.text = element_text(size = 14, color = 'black'),
-      axis.text = element_text(size = 12, color = 'black'),
-      axis.title = element_text(size = 14, color = 'black')
+      strip.text = element_text(size = 11, color = 'black'),
+      axis.text = element_text(size = 10, color = 'black'),
+      axis.title = element_text(size = 11, color = 'black')
     ) +
     labs(x = "VX34 genome position (Mb)", y = "Counts of TTAGGC per kb") +
     scale_y_continuous(expand = expansion(mult = c(0,0.1))) +
     coord_cartesian(ylim = c(0,180)),
   
   
-  nrow = 4
-)
+  nrow = 4)
+
+tel_count_plt
+
+# Save plot
+ggsave("../../figures/supplementary/telomere_density.png", tel_count_plt, width = 6, height = 7, dpi = 600)
+
