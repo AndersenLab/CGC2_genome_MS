98260d863eb7722f9075d323a3f3c3f54dc4e56d
lanceman999 <loconn13@jh.edu>
2026-04-14 19:59:46 -0400

    cb5 added to telomere plot

--- scripts/AF16cb5_QX1410_dotplot/AF16cb5_QX1410_dotplot.R
+++ scripts/AF16cb5_QX1410_dotplot/AF16cb5_QX1410_dotplot.R
@@ -18,7 +18,7 @@ afcb_qx_main <- ggplot(afcb_qx) +
   facet_wrap(~QX1410, scales = "free", ncol = 3) +
   scale_x_continuous(breaks = seq(0, 20, 5)) +
   scale_y_continuous(breaks = seq(0, 20, 5)) +
-  labs(x = "QX1410 genome coordinates (Mb)", y = "AF16 CB5 genome coordinates (Mb)") +
+  labs(x = "QX1410 genome coordinates (Mb)", y = "AF16 cb5 genome coordinates (Mb)") +
   theme(
     panel.border = element_rect(color = 'black', fill = NA),
     legend.position = "none",
--- scripts/telomeres/telomere_density.R
+++ scripts/telomeres/telomere_density.R
@@ -11,7 +11,12 @@ tel_ct_CGC2 <- readr::read_tsv("../../processed_data/genomes/CGC2_telomeres_binn
   dplyr::mutate(chrom = factor(chrom, levels = chrom_order), mid_Mb = (start + end) / 2 / 1e6) %>%
   dplyr::filter(!grepl(">AC186293.1","chrom"))
 
-# Laod in AF16 telomere counts
+# Load in AF16 cb5 telomere counts
+tel_ct_AF16cb5 <- readr::read_tsv("../../processed_data/genomes/AF16_cb5_telomeres_binned_1kb.bed", col_names = c("chrom","start","end","count")) %>%
+  dplyr::filter(chrom != "MtDNA" & !grepl("cb25", chrom)) %>%
+  dplyr::mutate(chrom = factor(chrom, levels = chrom_order), mid_Mb = (start + end) / 2 / 1e6)
+
+# Laod in AF16 cb4 telomere counts
 tel_ct_AF16 <- readr::read_tsv("../../processed_data/genomes/AF16_telomeres_binned_1kb.bed", col_names = c("chrom","start","end","count")) %>%
   dplyr::filter(chrom != "MtDNA" & !grepl("cb25", chrom)) %>%
   dplyr::mutate(chrom = factor(chrom, levels = chrom_order), mid_Mb = (start + end) / 2 / 1e6)
@@ -45,6 +50,22 @@ tel_count_plt <-
         scale_y_continuous(expand = expansion(mult = c(0,0.1))) +
         coord_cartesian(ylim = c(0,180)),
       
+      ggplot(data = tel_ct_AF16cb5) +
+        geom_col(aes(x = mid_Mb, y = count), color = '#1F78B4', width = 0.0001) +
+        facet_wrap(~chrom, nrow = 1, scales = "free_x") +
+        theme_bw() +
+        theme(
+          panel.border = element_rect(color = 'black', fill = NA),
+          strip.text = element_text(size = 11, color = 'black'),
+          axis.text = element_text(size = 9, color = 'black'),
+          axis.title = element_text(size = 11, color = 'black'),
+          plot.margin = margin(l = 20, r = 2, t = 2, b = 2),
+          axis.title.y = element_blank()
+        ) +
+        labs(x = "AF16 cb5 genome coordinates (Mb)") +
+        scale_y_continuous(expand = expansion(mult = c(0,0.1))) +
+        coord_cartesian(ylim = c(0,180)),
+      
       ggplot(data = tel_ct_AF16) +
         geom_col(aes(x = mid_Mb, y = count), color = '#E6B3B3', width = 0.0001) +
         facet_wrap(~chrom, nrow = 1, scales = "free_x") +
@@ -57,7 +78,7 @@ tel_count_plt <-
           plot.margin = margin(l = 20, r = 2, t = 2, b = 2),
           axis.title.y = element_blank()
         ) +
-        labs(x = "AF16 genome coordinates (Mb)") +
+        labs(x = "AF16 cb4 genome coordinates (Mb)") +
         scale_y_continuous(expand = expansion(mult = c(0,0.1))) +
         coord_cartesian(ylim = c(0,180)),
       
@@ -93,11 +114,11 @@ tel_count_plt <-
         scale_y_continuous(expand = expansion(mult = c(0,0.1))) +
         coord_cartesian(ylim = c(0,180)),
       
-      nrow = 4) +
+      nrow = 5) +
   draw_label("Counts of TTAGGC per kb", x=0.005, y=0.5, vjust= 1.5, angle=90, size = 11)
 
-tel_count_plt
+# tel_count_plt
 
 # Save plot
-ggsave("../../figures/supplementary/telomere_density.png", tel_count_plt, width = 7, height = 8, dpi = 600)
+ggsave("../../figures/supplementary/telomere_density.png", tel_count_plt, width = 7, height = 9, dpi = 600)
 
